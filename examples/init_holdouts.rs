e13b506c7e5bd735ee72e4729793ae02568da9d9
AIEN <aien.atlas@proton.me>
2026-09-19 21:23:54 -0500

    feat(examples): add init_holdouts helper to populate builtin test suites

---
+++ examples/init_holdouts.rs
@@ -0,0 +1,12 @@
+use spark_rsi::actor::judge::HoldoutSuite;
+use std::path::Path;
+
+fn main() {
+    let holdouts = Path::new(".rsi/holdouts");
+    std::fs::create_dir_all(holdouts).unwrap();
+    let suites = HoldoutSuite::builtin_suites();
+    for s in &suites {
+        s.save_to_dir(holdouts).unwrap();
+    }
+    println!("Populated .rsi/holdouts with {} builtin holdout suites", suites.len());
+}
